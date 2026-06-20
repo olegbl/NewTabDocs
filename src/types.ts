acf6f35540cdb1b6da97e97b647328e0d6fb754a
Oleg Lokhvitsky <olegbl@gmail.com>
2026-06-19 19:17:05 -0700

    feat: add shared TypeScript types

---
+++ src/types.ts
@@ -0,0 +1,29 @@
+export interface Tab {
+  id: string
+  content: string
+  updatedAt: number
+}
+
+export interface SyncMeta {
+  lastSyncedAt: number | null
+  lastSyncedDriveVersion: string | null
+}
+
+export type SyncStatus = 'idle' | 'syncing' | 'synced' | 'error' | 'disconnected'
+
+export interface DriveFile {
+  id: string
+  etag: string
+  modifiedTime: string
+}
+
+export interface DriveBackup {
+  tabs: Tab[]
+  savedAt: number
+}
+
+export interface ConflictState {
+  local: DriveBackup
+  remote: DriveBackup
+  remoteEtag: string
+}
